d757fe7bc1b6e03e99f0efa6db67d94394c5ea70
陈景元 <2084470563@qq.com>
2022-09-21 13:54:23 +0800

    init

---
+++ calculator/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (<div>
+      <a href="">1213</a>
+      <div className="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+    </div>
+  )
+}
+
+export default App
